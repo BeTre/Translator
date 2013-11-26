2726c32389034a3c9a06ac4a83dddc86b5882d96
BeTre <benjamin.treiber@gmail.com>
2013-11-26 12:55:57 +0100

    various changes

--- server.py
+++ server.py
@@ -16,6 +16,10 @@ import csv
 
 app = Flask(__name__)
 
+csv_delimiter = ','
+csv_quotechar = "'"
+
+
 @app.route('/')
 def index():
     f = open('index.html', 'w')
@@ -30,7 +34,7 @@ def index():
     </tr>\n''')
 
     with open('vocabulary_list.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile, delimiter=csv_delimiter, quotechar=csv_quotechar)
         for row in reader:
             f.write('<tr>\n')
             for element in row:
@@ -48,47 +52,36 @@ def index():
 
 @app.route('/new')
 def new():
-	a = 'hallo'
-	return a
+    a = 'hallo'
+    return a
 
 @app.route('/learn/<int:learn_id>', methods=['GET'])
 def show_learn(learn_id):
-	readin = csv.reader(open('vocabulary_list.csv', 'r'))
-	voclist = list(readin)
+    readin = csv.reader(open('vocabulary_list.csv', 'r'), delimiter=csv_delimiter, quotechar=csv_quotechar)
+    voclist = list(readin)
 
+    site = '''<!DOCTYPE html>
+    <html>
+    <body>
+    <h3>Vokabeltraining</h3>\n'''+voclist[learn_id][0]+'\n'+'''<form name='input'
+    action='/learn/'''+str(learn_id)+''''
+    method='get'>Übersetzung:<input type='text' name='vocabulary'>
+    <input type='submit' value='Submit'>
+    </form>'''+'\n'+'\n'+'''<a href='/learn/{nextpage}'>nächste Vokabel</a>'''.format(nextpage=learn_id+1)
+    if 'vocabulary' in request.args:
+        vocabulary = request.args['vocabulary']
+        if unicode(vocabulary).encode('utf-8') == voclist[learn_id][1]:
+            site += 'RICHTIG'
+            voclist[learn_id][2] = 1
+        else:
+            site += 'FALSCH'+'\n'+unicode(vocabulary).encode('utf-8')+'\n'
+            voclist[learn_id][2] = 0
 
-	f = open('learn.html', 'w')
-	f.write('''<!DOCTYPE html>
-	<html>
-	<body>
-	<h3>Vokabeltraining</h3>\n''')
-	f.write(voclist[learn_id][0]+'\n')
-	f.write('''<form name='input'
-	action='/learn/'''+str(learn_id)+''''
-	method='get'>Übersetzung:<input type='text' name='vocabulary'>
-	<input type='submit' value='Submit'>
-	</form>''')
-	f.write('\n')
-	f.write('''<a href='/learn/%i'>nächste Vokabel</a>'''%(learn_id+1))
-	vocabulary = request.args.get('vocabulary')
-	if unicode(vocabulary).encode('utf-8') == voclist[learn_id][1]:
-		f.write('RICHTIG')
-		voclist[learn_id][2] = 1
-	else:
-		f.write('FALSCH\n')
-		f.write(unicode(vocabulary).encode('utf-8')+'\n')
-		voclist[learn_id][2] = 0
-	f.close()
-
-
-	with open('vocabulary_list2.csv', 'w') as csvfile:
-		readout = csv.writer(csvfile)
-		for line in voclist:
-			readout.writerow(line)
-
-	f = open('learn.html', 'r')
-	return f.read()
-
+    with open('vocabulary_list.csv', 'w') as csvfile:
+        readout = csv.writer(csvfile, delimiter=csv_delimiter, quotechar=csv_quotechar)
+        for line in voclist:
+            readout.writerow(line)
+    return site
 
 
 if __name__ == '__main__':
