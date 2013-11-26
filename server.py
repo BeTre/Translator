c9ec59884cb2e6f70f6f34e033f06dd4bdd2fc92
BeTre <benjamin.treiber@gmail.com>
2013-11-26 15:33:26 +0100

    Implementation of templates

--- server.py
+++ server.py
@@ -11,77 +11,59 @@
 
 from flask import Flask
 from flask import request
-from numpy import loadtxt
+from flask import render_template
 import csv
 
 app = Flask(__name__)
 
 csv_delimiter = ','
-csv_quotechar = "'"
+csv_quotechar = "\""
+input_file = 'vocabulary_list.csv'
+output_file = 'vocabulary_list2.csv'
+
+def readin_csv(input_file):
+    '''read in utf-8 csv file and return unicode list'''
+    with open(input_file, 'r') as csvfile:
+        readin = csv.reader(csvfile, delimiter=csv_delimiter, quotechar=csv_quotechar)
+        voclist = []
+        for line in readin:
+            line_decoded = []
+            for element in line:
+                line_decoded.append(element.decode('utf-8'))
+            voclist.append(line_decoded)
+        return voclist
 
 
 @app.route('/')
 def index():
-    f = open('index.html', 'w')
-    f.write('''<!DOCTYPE html>
-    <html>
-    <body>
-    <h3>Vokabelliste</h3>
-    <table border='1'>
-    <tr>
-    <th>Deutsch</th>
-    <th>Schwedisch</th>
-    </tr>\n''')
+    voclist = readin_csv(input_file)
+    return render_template('list.html', voclist = voclist)
 
-    with open('vocabulary_list.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter=csv_delimiter, quotechar=csv_quotechar)
-        for row in reader:
-            f.write('<tr>\n')
-            for element in row:
-                f.write('<td>')
-                f.write(element)
-                f.write('</td>\n')
-            f.write('</tr>\n')
-    f.write('''
-    </table>
-    </body>
-    </html>''')
-    f.close()
-    f = open('index.html', 'r')
-    return f.read()
-
-@app.route('/new')
-def new():
-    a = 'hallo'
-    return a
 
 @app.route('/learn/<int:learn_id>', methods=['GET'])
-def show_learn(learn_id):
-    readin = csv.reader(open('vocabulary_list.csv', 'r'), delimiter=csv_delimiter, quotechar=csv_quotechar)
-    voclist = list(readin)
+def learn(learn_id):
+    voclist = readin_csv(input_file)
 
-    site = '''<!DOCTYPE html>
-    <html>
-    <body>
-    <h3>Vokabeltraining</h3>\n'''+voclist[learn_id][0]+'\n'+'''<form name='input'
-    action='/learn/'''+str(learn_id)+''''
-    method='get'>Übersetzung:<input type='text' name='vocabulary'>
-    <input type='submit' value='Submit'>
-    </form>'''+'\n'+'\n'+'''<a href='/learn/{nextpage}'>nächste Vokabel</a>'''.format(nextpage=learn_id+1)
     if 'vocabulary' in request.args:
         vocabulary = request.args['vocabulary']
-        if unicode(vocabulary).encode('utf-8') == voclist[learn_id][1]:
-            site += 'RICHTIG'
-            voclist[learn_id][2] = 1
+        if vocabulary == voclist[learn_id][1]:
+            voclist[learn_id][2] = '1'
+            correct = True
         else:
-            site += 'FALSCH'+'\n'+unicode(vocabulary).encode('utf-8')+'\n'
-            voclist[learn_id][2] = 0
+            voclist[learn_id][2] = '0'
+            correct = voclist[learn_id][1]
+        with open(output_file, 'w') as csvfile:
+            readout = csv.writer(csvfile, delimiter=csv_delimiter, quotechar=csv_quotechar)
+            for line in voclist:
+                line2 = []
+                for element in line:
+                    line2.append(element.encode('utf-8'))
+                readout.writerow(line2)
+    else:
+        correct = 'start'
 
-    with open('vocabulary_list.csv', 'w') as csvfile:
-        readout = csv.writer(csvfile, delimiter=csv_delimiter, quotechar=csv_quotechar)
-        for line in voclist:
-            readout.writerow(line)
-    return site
+
+    return render_template('learn.html', voc_to_translate = voclist[learn_id][0], learn_id = learn_id, correct = correct)
 
 
 if __name__ == '__main__':
